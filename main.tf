b06d08d3ee1696ed1b61999c8d6acf95dca3d40a
Alexandre Debargis <adebargis@microsoft.com>
2023-09-04 23:26:55 -0400

    main tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+    cloud {
+        organization = "ALEXANDREDEBARGIS-training"
+
+        workspaces {
+            name = "azure-storage-account-creation"
+        }
+    }
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 3.59.0"
+        }
+    }
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = "true"
+
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = var.stg_name
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.replication_type
+
+  tags = {
+    environment = var.environment
+  }
+}
